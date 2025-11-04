07b00c7069bd3a1a5b079f968bd97063b21aa6a0
wasawatwaladee <wasawatwaladee2@gmail.com>
2025-11-04 14:44:35 +0700

    Delete Post done

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import createHttpError from "http-errors"
 import prisma from "../config/prisma.config.js"
 
 export const getAllPosts= async(req,res)=>{
@@ -6,7 +7,15 @@ export const getAllPosts= async(req,res)=>{
         include:{
             user:{select:{
                 firstName:true,lastName:true,profileImage:true
-            }}
+            }},
+            comments: {
+                include:{
+                    user:{select:{
+                      firstName:true,lastName:true,profileImage:true  
+                    }}
+                }
+            },
+            likes: true
         }
     })
     res.json({posts:result})
@@ -22,4 +31,28 @@ export const createPost = async(req , res ,next)=>{
         message:'Create new Post done',
         result
         })
+}
+
+export const deletePost = async(req,res,next)=>{
+   const {id} = req.params
+
+   const foundPost = await prisma.post.findUnique({
+    where:{id: Number(id)}
+   })
+   if(!foundPost){
+    return next(createHttpError[404]("Data not found"))
+   }
+
+   if(req.user.id !== foundPost.userId){
+    return next(createHttpError[401]("Cannot delete this post"))
+   }
+
+   const result = await prisma.post.delete({
+    where:{id:Number(id)}
+   })
+
+   res.json({
+    message: "Delete done"
+   })
+
 }
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createPost, getAllPosts } from "../controllers/post.controller.js";
+import { createPost, deletePost, getAllPosts } from "../controllers/post.controller.js";
 
 const postRoute = Router();
 
 postRoute.get('/',getAllPosts)
 postRoute.post('/',createPost)
+postRoute.delete('/:id',deletePost)
 
 export default postRoute;
